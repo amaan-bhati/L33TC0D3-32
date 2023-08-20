82f4f8c9d99e81fbd3f060e811419f6cef981559
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-08-20 23:07:45 +0530

    Update n-ary-tree postorder traversal.js

--- n-ary-tree postorder traversal.js
+++ n-ary-tree postorder traversal.js
@@ -1,1 +1,31 @@
 
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var postorder = function (root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const stack = [root];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        result.unshift(node.val); // Insert at the beginning of the result array
+        for (const child of node.children) {
+            stack.push(child);
+        }
+    }
+
+    return result;
+};
