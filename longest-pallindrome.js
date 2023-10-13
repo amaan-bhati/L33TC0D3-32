64c95dbd2fb207a0a55e26f176ba61a74d282852
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-13 23:49:28 +0530

    Update longest-pallindrome.js

--- longest-pallindrome.js
+++ longest-pallindrome.js
@@ -1,1 +1,29 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    const frequencyMap = new Map();
+    
+    for (const char of s) {
+        if (frequencyMap.has(char)) {
+            frequencyMap.set(char, frequencyMap.get(char) + 1);
+        } else {
+            frequencyMap.set(char, 1);
+        }
+    }
+    
+    let oddCount = 0;
+    let evenCount = 0;
+    
+    for (const count of frequencyMap.values()) {
+        if (count % 2 === 0) {
+            evenCount += count;
+        } else {
+            oddCount += count - 1;
+        }
+    }
+    const length = evenCount + (oddCount > 0 ? 1 : 0);
+    
+    return length;
+};
