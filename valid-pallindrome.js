697dcdaf24d39f8887ec784200b4235bdc3c953d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-11-03 23:36:11 +0530

    Update valid-pallindrome.js

--- valid-pallindrome.js
+++ valid-pallindrome.js
@@ -1,7 +1,4 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
+
 var isPalindrome = function (s) {
     // Convert to lowercase and remove non-alphanumeric characters
     s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
