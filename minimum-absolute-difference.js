364a28b52f9ea9a21bcb77ec1cd2d1f0351ab75f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-18 22:54:48 +0530

    Update minimum-absolute-difference.js

--- minimum-absolute-difference.js
+++ minimum-absolute-difference.js
@@ -1,1 +1,18 @@
 
+var minimumAbsDifference = function (arr) {
+    arr = arr.sort((a, b) => a - b)
+    let sol = []
+    let min = arr[1] - arr[0]
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i + 1] - arr[i] < min) {
+            min = arr[i + 1] - arr[i]
+            sol = []
+        }
+        if (arr[i + 1] - arr[i] == min) sol.push([arr[i], arr[i + 1]])
+    }
+
+    if (min == arr[1] - arr[0]) sol.unshift([arr[0], arr[1]])
+
+    return sol
+}; 
